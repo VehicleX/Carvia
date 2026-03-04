25f1138b0f8505480ee6fd5c3252dc5a3801d30f
Dinesh <dineshveerabhargav@gmail.com>
2026-03-04 13:32:11 +0530

    ensure generated secrets are created before building app

--- build.sh
+++ build.sh
@@ -21,5 +21,28 @@ class ApiKeys {
 }
 EOF
 
+echo "Generating missing firebase_options.dart file..."
+cat <<EOF > lib/firebase_options.dart
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) return web;
+    throw UnsupportedError('DefaultFirebaseOptions are not supported for this platform.');
+  }
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyD8g-vIbrF4ElDv8vQIAErfDiJ3q3a-Jb8',
+    appId: '1:90676120528:web:e8173ea885d482153a87d5',
+    messagingSenderId: '90676120528',
+    projectId: 'carvia-88040',
+    authDomain: 'carvia-88040.firebaseapp.com',
+    storageBucket: 'carvia-88040.firebasestorage.app',
+    measurementId: 'G-748VLK0MYB',
+  );
+}
+EOF
+
 echo "Building for web..."
 flutter build web --release --no-tree-shake-icons --no-source-maps --web-renderer canvaskit
